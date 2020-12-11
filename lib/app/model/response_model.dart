c06aa6ae6405c8c8b837257b02a11ae2044f12dd
ittronDev <haristhohir@gmail.com>
2020-12-12 01:47:42 +0700

    fix response model

--- lib/app/model/response_model.dart
+++ lib/app/model/response_model.dart
@@ -17,29 +17,11 @@ class ResponseModel<T extends AbstractDataModel> implements AbstractModel {
 
   ResponseModel();
 
-  /// Add factory functions for every Type and every constructor you want to make available to `make`
-
-  static Map<Type, Function> factories =  {
-    PaginationDataModel: (Map map, Function(Map) factoryBuilder) => PaginationDataModel.fromJson(map,factoryBuilder),
-  };
-
-  static registerFactory(Type t,Function f) {
-    factories[t]=f;
-  }
 
   ResponseModel.fromJson(Map<String,dynamic> json, Function(Map) factoryBuilder) {
     errCode = Arr.getInt(json,'errCode');
     errMessage = Arr.getString(json,'errMessage');
-    //if(T.toString().startsWith("PaginationDataModel")) {
-      //data = factories[PaginationDataModel](Arr.getMap(json, 'data'), factoryBuilder);
-      //data = PaginationDataModel<T>.fromJson(Arr.getMap(json, 'data'), factoryBuilder);
-    //} else {
-      //if (factories.containsKey(T)) {
-        //data = factories[T](Arr.getMap(json, 'data'), factoryBuilder);
-      //} else {
-        data = factoryBuilder(Arr.getMap(json, 'data'));
-      //}
-    //}
+    data = factoryBuilder(Arr.getMap(json, 'data'));
 
 
   }
