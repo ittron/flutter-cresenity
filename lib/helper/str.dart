ee58dbc63e798bf32b513803636f3145b18c45a0
Hery <hery@ittron.co.id>
2020-12-12 05:03:38 +0700

    update Str helpers and String extension

--- lib/helper/str.dart
+++ lib/helper/str.dart
@@ -2,9 +2,11 @@
 
 
 class Str {
-  /**
-   * 
-   */
+  /// Return the remainder of a string after the first occurrence of a given value.
+  ///
+  /// @param subject
+  /// @param search
+  /// @return The new string after first match
   static String after(String subject, String search) {
 
     return search == '' ? subject : subject.split(search).sublist(1).join(search);
--- test/string_test.dart
+++ test/string_test.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter_cresenity/helper/str.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_cresenity/extension/string_extension.dart';
 
 void main() {
 
@@ -12,6 +13,10 @@ void main() {
 
     String str4word = "Hello world is awesome";
     expect(Str.after(str4word, " "),"world is awesome");
+
+    //test extension
+    expect(str4word.after(" "),"world is awesome");
+
   });
 
 }
