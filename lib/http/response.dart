cce2d2c2eac9fb778dc7cdcfcd362931971a9dc1
Hery <hery@ittron.co.id>
2020-12-12 04:16:41 +0700

    add method String on response

--- lib/http/response.dart
+++ lib/http/response.dart
@@ -6,4 +6,10 @@ class Response {
   Response({this.body,this.statusCode});
 
 
+  String toString() {
+    return "CF.Http.Response " + {
+      "statusCode":statusCode,
+      "body":body
+    }.toString();
+  }
 }
