3f8418ca32434823bf2ccd936b57495b7471567d
Hery <hery@ittron.co.id>
2020-12-12 04:17:22 +0700

    add model_test

---
+++ test/http_test.dart
@@ -0,0 +1,59 @@
+
+
+library flutter_cresenity;
+
+
+
+import 'dart:convert';
+
+import 'package:flutter_cresenity/http/response.dart';
+import 'package:flutter_cresenity/support/collection.dart';
+import 'package:flutter_cresenity/cf.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/support/array.dart';
+
+void main() async {
+  test('One Record test', () async {
+
+    Collection files = Collection();
+    Collection params = Collection();
+    String url = 'https://jsonplaceholder.typicode.com/todos/1';
+    Response response = await CF.http.waitRequest(
+      url: url,
+      method: 'get',
+      data: params,
+      files: files,
+    );
+
+    print(response);
+  });
+
+
+  test('List Record test', () async {
+
+    Collection files = Collection();
+    Collection params = Collection();
+    String url = 'https://jsonplaceholder.typicode.com/users';
+    Response response = await CF.http.waitRequest(
+      url: url,
+      method: 'get',
+      data: params,
+      files: files,
+    );
+
+    Array users = new Array(jsonDecode(response.body));
+
+    print(users[0]);
+    expect(users[0] is Map, true);
+
+    expect(users[11] == null, true);
+
+
+
+  });
+
+
+
+}
+
---
+++ test/model_test.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter_cresenity/app/model/response_model.dart';
+import 'package:flutter_cresenity/app/model/response_pagination_model.dart';
+import 'package:flutter_cresenity/app/model/pagination_data_model.dart';
+import 'package:flutter_cresenity/app/model/abstract_data_model.dart';
+import 'package:flutter_cresenity/helper/arr.dart';
+
+
+
+
+class PostModel extends AbstractDataModel {
+
+  int postId;
+  @override
+  Map<String, dynamic > toJson() {
+    return {'postId':postId};
+  }
+
+  PostModel.fromJson(Map map) {
+    postId = map['postId'];
+  }
+
+}
+
+
+void main() {
+  Map<String,dynamic> data = {
+    'errCode':0,
+    'errMessage':'',
+    'data': {
+      'total':3,
+      'lastPage':1,
+      'perPage':10,
+      'currentPage':1,
+      'items': [{
+        'postId':1
+      },
+        {
+          'postId':2
+        },
+        {
+          'postId':3
+        }]
+    }
+  }; //from api
+
+  /*
+  ResponseModel<PaginationDataModel<PostModel>> response =ResponseModel<PaginationDataModel<PostModel>>.fromJson(data, (item) {
+    return PaginationDataModel<PostModel>.fromJson(item, (postItem) {
+      return PostModel.fromJson(postItem);
+    });
+
+  });
+  */
+  ResponsePaginationModel<PostModel> response =ResponsePaginationModel<PostModel>.fromJson(data, (item) {
+      return PostModel.fromJson(item);
+
+  });
+
+  response.data.items.forEach((element) {
+    print(element.postId);
+  });
+}
