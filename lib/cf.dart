b070ea75fa4c63dd3ddfa50fe51ec1924d2a5225
Hery <hery@ittron.co.id>
2020-12-12 04:17:13 +0700

    remove unused import

--- lib/cf.dart
+++ lib/cf.dart
@@ -1,8 +1,5 @@
 library flutter_cresenity;
 
-import 'package:flutter_cresenity/app/model/abstract_data_model.dart';
-import 'package:flutter_cresenity/app/model/pagination_data_model.dart';
-import 'package:flutter_cresenity/app/model/response_model.dart';
 import 'package:flutter_cresenity/bloc/bloc_manager.dart';
 import 'package:flutter_cresenity/http/http.dart';
 import 'package:flutter_cresenity/router/navigator.dart';
