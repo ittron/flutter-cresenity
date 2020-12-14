179f7cf9cc239707ee1b4cd68f0852ba48e465f6
ittronDev <haristhohir@gmail.com>
2020-12-14 23:09:10 +0700

    update

--- lib/app/model/collection_data_model.dart
+++ lib/app/model/collection_data_model.dart
@@ -4,16 +4,15 @@
 import 'package:flutter_cresenity/app/model/abstract_data_model.dart';
 import 'package:flutter_cresenity/support/collection.dart';
 
-class CollectionDataModel extends Collection implements AbstractDataModel {
-  CollectionDataModel([items]): super(items);
-
+class CollectionDataModel extends AbstractDataModel{
+  Collection items;
   CollectionDataModel.fromJson(Map<String,dynamic> map) {
-    setItems(map);
+    this.items = Collection(map);
 
   }
 
 
   @override
-  Map<String, dynamic > toJson() => all();
+  Map<String, dynamic > toJson() => items.all();
 
 }
