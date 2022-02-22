37a25015dbd3fc56d8ab226555d848aa8982700c
Suhardik Nirmansyah <suhardik.dev@gmail.com>
2022-02-22 17:57:10 +0700

    update dependencies

--- lib/utils/toast.dart
+++ lib/utils/toast.dart
@@ -1,2 +1,13 @@
-/// @author Suhardik Nirmansyah 
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart' as fToast;
+
+/// @author Suhardik Nirmansyah
 /// @since  22/02/2022
+
+class Toast {
+  static show(String message) {
+    fToast.Fluttertoast.showToast(
+      msg: message,
+    );
+  }
+}
