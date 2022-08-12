c4d4f9ceb3aa767d5f9fc039c42dba647e2fc0af
ipul <ipul@ittron.id>
2022-08-12 21:14:51 +0700

    update

--- lib/validation/closure_rule.dart
+++ lib/validation/closure_rule.dart
@@ -1,22 +1,17 @@
-
-
-
 import 'package:flutter_cresenity/validation/rule_interface.dart';
 
 class ClosureRule implements RuleInterface {
-
   Function callback;
 
   bool failed = false;
 
-
-  String msg;
+  String msg = '';
 
   ClosureRule(this.callback);
 
   bool passes(String attribute, value) {
     failed = false;
-    this.callback(attribute,value,(message){
+    this.callback(attribute, value, (message) {
       this.failed = true;
       this.msg = message;
     });
